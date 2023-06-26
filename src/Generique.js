56aad986e90d7850e7897afb923028aacd4b78cb
Mat0108 <61587759+Mat0108@users.noreply.github.com>
2023-06-26 19:53:18 +0200

    commit 2

--- src/Generique.js
+++ src/Generique.js
@@ -1,9 +1,51 @@
+<<<<<<< HEAD
 
 export class Terrain{
     constructor(x,y){
         this._x = x;
         this._y = y;
     }
+=======
+import React from "react";
+
+export class Terrain extends React.Component{
+    constructor(props){
+        super(props)
+        this._x = props.x;
+        this._y = props.y;
+        this._grille = new Array(props.y).fill(0).map(() => new Array(props.x).fill({case:null,defense:null,unité:null,action:null}));   
+        this._grille[3][4] = {case:new Position(3,4),defense:null,unité:null};
+        this._terrain = props.terrain
+        this._list = []
+    }
+
+    componentDidUpdate(props){
+        if(props._list != this._list){
+            this._list = props._list;
+        }
+    }
+    render(){
+        return <div className="relative">
+        <div className="absolute z-[0]"><img src={this._terrain} alt={"terrain"}/></div>
+        <div className="absolute flex flex-col z-[2000] top-[65px] left-[10px]">
+        {this._grille.map((e,pos)=>{
+          
+            
+            return <div className={`${pos % 2 == 1 ? "ml-[45px]":""} w-full h-[79px] flex flex-row`}>{
+                e.map((f,pos2)=>{
+
+                    if(pos2 != (pos % 2 == 1 ? this._x-1 : this._x)){
+                        
+                        return <div className={`w-[91px] h-[79px] border-2 border-black ${this._list.map(e=>{if(e.x == pos && e.y == pos2){return "bg-red"}})} ${(pos == 3 && pos2 == 4 )?"bg-green":""}`} onClick={()=>{if(pos == 3 && pos2 == 4){this._list = f.case.CaseProche()}}}>{}</div>
+                    
+                    }})
+            }</div>
+            
+        })}
+    </div></div>
+    }
+
+>>>>>>> 2331620 (commit)
     getTerrainX(){
         return this._x;
     }
@@ -12,7 +54,11 @@ export class Terrain{
     }
 }
 export class Position{
+<<<<<<< HEAD
     constructor(posx,posy,x,y){
+=======
+    constructor(posx,posy){
+>>>>>>> 2331620 (commit)
         this._posx = posx;
         this._posy = posy;
     }
@@ -111,6 +157,35 @@ export class Position{
             list.push({x:this._posx,y:this._posy-3,dés:dés[2]});
             list.push({x:this._posx+1,y:this._posy-3,dés:dés[2]});
         }
+        if(portée >=4){
+            list.push({x:this._posx-4,y:this._posy,dés:dés[3]}); //partie gauche du cercle
+            list.push({x:this._posx-4,y:this._posy-1,dés:dés[3]});
+            list.push({x:this._posx-4,y:this._posy+1,dés:dés[3]});
+            list.push({x:this._posx-3,y:this._posy-2,dés:dés[3]});
+            list.push({x:this._posx-3,y:this._posy+2,dés:dés[3]});
+            list.push({x:this._posx-3,y:this._posy-3,dés:dés[3]});
+            list.push({x:this._posx-3,y:this._posy+3,dés:dés[3]});
+
+            list.push({x:this._posx+4,y:this._posy,dés:dés[3]}); //partie droite du cercle
+            list.push({x:this._posx+3,y:this._posy-1,dés:dés[3]});
+            list.push({x:this._posx+3,y:this._posy+1,dés:dés[3]});
+            list.push({x:this._posx+3,y:this._posy-2,dés:dés[3]});
+            list.push({x:this._posx+3,y:this._posy+2,dés:dés[3]});
+            list.push({x:this._posx+2,y:this._posy-3,dés:dés[3]});
+            list.push({x:this._posx+2,y:this._posy+3,dés:dés[3]});
+
+            list.push({x:this._posx-2,y:this._posy+4,dés:dés[3]}); //partie haute du cercle
+            list.push({x:this._posx-1,y:this._posy+4,dés:dés[3]});
+            list.push({x:this._posx,y:this._posy+4,dés:dés[3]});
+            list.push({x:this._posx+1,y:this._posy+4,dés:dés[3]});
+            list.push({x:this._posx+2,y:this._posy+4,dés:dés[3]});
+
+            list.push({x:this._posx-2,y:this._posy-4,dés:dés[3]}); //partie basse du cercle
+            list.push({x:this._posx-1,y:this._posy-4,dés:dés[3]});
+            list.push({x:this._posx,y:this._posy-4,dés:dés[3]});
+            list.push({x:this._posx+1,y:this._posy-4,dés:dés[3]});
+            list.push({x:this._posx+2,y:this._posy-4,dés:dés[3]});
+        }
         let updatelist = []
         list.map(e=>{
             if(e.x >= 0 && e.x < 13 && e.y >= 0 && e.y < 9){
@@ -121,17 +196,76 @@ export class Position{
     }
 
 }
-export class SoldatGenerique {
-    constructor(nombre,vie,portée,deplacementavectir,deplacementsanstir,image,posx,posy){
+export class SoldatGenerique  {
+    // constructor(nombre,vie,portée,deplacementavectir,deplacementsanstir,image,posx,posy,clan){
+    //     this._nombre = nombre;
+    //     this._vie = vie;
+    //     this._portée = portée;
+    //     this._deplacementavectir = deplacementavectir;
+    //     this._deplacementsanstir = deplacementsanstir;
+    //     this.image = image;
+    //     this._pos = Position(posx,posy);
+    //     this._clan = clan; 
+
+    // }
+    constructor(image,nombre,vie){
+        
+        this._image = image;
         this._nombre = nombre;
         this._vie = vie;
-        this._portée = portée;
-        this._deplacementavectir = deplacementavectir;
-        this._deplacementsanstir = deplacementsanstir;
-        this.image = image;
-        this._pos = Position(posx,posy);
-        this._clan = clan; 
-
+    }
+    render(){
+        if(this._nombre == 4){
+            let elem = <img src={this._image} alt={"Soldat"} className="w-1/2"/>
+                        
+            return (
+            <div className="flex  w-full">
+                <div className="absolute top-2 w-full">
+                    <div className="flex flex-row ">
+                        {this._vie > 0 ? elem:""}
+                        {this._vie > 1 ? elem:""}
+                    </div>
+                </div>
+                <div className="absolute top-12 z-[40]">
+                    <div className="flex flex-row">                    
+                        {this._vie > 2 ? elem:""}
+                        {this._vie > 3 ? elem:""}
+                    </div>
+                </div>
+            </div>)
+        }
+        if(this._nombre == 3){
+            let elem = <img src={this._image} alt={"Soldat"} className="w-1/2"/>
+                        
+            return (
+            <div className="flex  w-full">
+                <div className="absolute top-2 w-full">
+                    <div className="flex flex-row center ">
+                        {this._vie > 0 ? elem:""}
+                    </div>
+                </div>
+                <div className="absolute top-12 z-[40]">
+                    <div className="flex flex-row">                    
+                        {this._vie > 1 ? elem:""}
+                        {this._vie > 2 ? elem:""}
+                    </div>
+                </div>
+            </div>)
+        }
+        if(this._nombre == 2){
+            let elem = <img src={this._image} alt={"Soldat"} className="w-1/2"/>
+                        
+            return (
+            <div className="flex w-full">
+                <div className="absolute top-6 z-[40]">
+                    <div className="flex flex-row">                    
+                        {this._vie > 0 ? elem:""}
+                        {this._vie > 1 ? elem:""}
+                    </div>
+                </div>
+            </div>)
+        }
+        return <div></div>
     }
     AfficherDeplacementAvecTir(){ 
         return this._deplacementavectir ? this._pos.CaseProche(this._deplacementavectir) : [];       
@@ -143,10 +277,12 @@ export class SoldatGenerique {
 }
 
 export class CaseGenerique {
-    constructor(image, malus,pos ) {
+    constructor(image ) {
         this._image = image;
-        this._malus = malus;
-        this._pos = pos;
+        this._malus = 0;
+    }
+    render(){
+        return <div><img src={this._image} alt={"Case"}/></div>
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Terrain,CaseGenerique,SoldatGenerique} from './Generique'
+function App() {
+  let x = 13;
+  let y = 9;
+  let grille = new Array(y).fill(0).map(() => new Array(x).fill({case:null,defense:null,unité:null,action:null}));   
+  grille[1][3] = {case: new CaseGenerique("images/base/h_hills.png"),defense:null,unité:null,action:null}
+  grille[1][4] = {case: new CaseGenerique("images/base/h_hills.png"),defense:null,unité:null,action:null}
+  grille[5][3] = {case: new CaseGenerique("images/base/h_hills.png"),defense:null,unité: new SoldatGenerique("images/base/u_gerinf_bottom.png",4,4),action:null}
+  grille[7][8] = {case: new CaseGenerique("images/base/h_forest.png"),defense:new CaseGenerique("images/base/o_sand2.png"),unité: new SoldatGenerique("images/base/u_ustank_bottom.png",3,3),action:null}
+  grille[2][10] = {case: new CaseGenerique("images/base/h_river1.png"),defense:new CaseGenerique("images/base/o_sand2.png"),unité: new SoldatGenerique("images/base/u_usgun_bottom.png",2,2),action:null}
+  
+  const global = useMemo(()=>{
+    {
+      return (
+      <div className="relative">
+        <div className="absolute z-[0]"><img src={"images/terrain0.png"} alt={"terrain"}/></div>
+        <div className="absolute flex flex-col z-[2000] top-[58px] left-[10px]">
+          {grille.map((e,pos)=>{
+            return <div className={`${pos % 2 == 1 ? "ml-[45px]":""} w-full flex flex-row`}>{
+              e.map((f,pos2)=>{
 
+                  if(pos2 != (pos % 2 == 1 ? x-1 : x)){
+                      
+                      return <div className={`relative w-[91px] h-[78px] border-0 border-white relative`} >
+                        <div className='absolute z-10 w-full h-full'>{f.case ? f.case.render(): ""}</div>
+                        <div className='absolute z-20 w-full h-full'>{f.defense ? f.defense.render(): ""}</div>
+                        <div className='absolute z-30 w-full h-full'>{f.unité ? f.unité.render(): ""}</div>
+                        
+                        </div>
+                  
+              }})
+          }</div>
+        })}
+        </div>
+      </div>)
+      }
+     },[grille])
+  return (
+    <div className="App w-full h-full relative bg-[#EEE8E4]">
+        {global}
+      </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
